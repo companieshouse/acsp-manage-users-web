cdca7e6b14cd7fb7b7b9a629663e50d56b937464
Maciej Modzelewski <150266545+mmodzelewski-ch@users.noreply.github.com>
2024-06-24 14:28:04 +0100

    IDVA6-1175 Service unavailable template used if any error present

--- src/app.ts
+++ src/app.ts
@@ -56,19 +56,19 @@ enableI18next(app);
 routerDispatch(app);
 
 // Unhandled errors
-app.use((err: any, req: Request, res: Response, _next: NextFunction) => {
+app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
     logger.error(`${err.name} - appError: ${err.message} - ${err.stack}`);
-    res.render("partials/error");
+    res.render(constants.SERVICE_UNAVAILABLE_TEMPLATE);
 });
 
 // Unhandled exceptions
-process.on("uncaughtException", (err: any) => {
+process.on("uncaughtException", (err: Error) => {
     logger.error(`${err.name} - uncaughtException: ${err.message} - ${err.stack}`);
     process.exit(1);
 });
 
 // Unhandled promise rejections
-process.on("unhandledRejection", (err: any) => {
+process.on("unhandledRejection", (err: Error) => {
     logger.error(`${err.name} - unhandledRejection: ${err.message} - ${err.stack}`);
     process.exit(1);
 });
--- src/router.dispatch.ts
+++ src/router.dispatch.ts
@@ -3,10 +3,10 @@ import { Application, Request, Response } from "express";
 import router from "./routers/router";
 import * as constants from "./lib/constants";
 
-const routerDispatch = (app: Application) => {
+const routerDispatch = (app: Application): void => {
     app.use(constants.LANDING_URL, router);
     app.use("*", (req: Request, res: Response) => {
-        res.status(404).render("partials/error");
+        res.status(404).render(constants.SERVICE_UNAVAILABLE_TEMPLATE);
     });
 };
 
