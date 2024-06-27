bc4c625063a8467a13006bfad051983febf4b2a4
Maciej Modzelewski <150266545+mmodzelewski-ch@users.noreply.github.com>
2024-06-27 11:29:00 +0100

    IDVA6-1179 Manage users page content created

--- src/routers/controllers/manageUsersController.ts
+++ src/routers/controllers/manageUsersController.ts
@@ -1,8 +1,38 @@
 import { Request, Response } from "express";
 import * as constants from "../../lib/constants";
 import { getTranslationsForView } from "../../lib/utils/translationUtils";
+import { AnyRecord } from "types/utilTypes";
+import { TableEntry } from "../../types/viewTypes";
+import { getLink } from "../../lib/utils/viewUtils";
 
 export const manageUsersControllerGet = async (req: Request, res: Response): Promise<void> => {
+    const viewData = getViewData(req);
+    res.render(constants.MANAGE_USERS_PAGE, { ...viewData });
+};
+
+const getViewData = (req: Request): AnyRecord => {
     const translations = getTranslationsForView(req.t, constants.MANAGE_USERS_PAGE);
-    res.render(constants.MANAGE_USERS_PAGE, { lang: translations });
+
+    // Hardcoded data will be replaced once relevant API calls available
+    const companyName = "MORRIS ACCOUNTING LTD";
+    const companyNumber = "0122239";
+    const userEmailAddress = "james.morris@gmail.com";
+    const userName = "James Morris";
+    const accountOwnersTableData: TableEntry[][] = [
+        [
+            { text: userEmailAddress },
+            { text: userName },
+            { html: getLink(constants.REMOVE_USER_FULL_URL, translations.remove as string) }
+        ]
+    ];
+
+    return {
+        lang: translations,
+        backLinkUrl: constants.DASHBOARD_FULL_URL,
+        removeUserLinkUrl: constants.REMOVE_USER_FULL_URL,
+        addUserUrl: constants.ADD_USER_FULL_URL,
+        companyName,
+        companyNumber,
+        accountOwnersTableData
+    };
 };
