82a413b4b7e830a4192132de2685141094dd3674
Maciej Modzelewski <150266545+mmodzelewski-ch@users.noreply.github.com>
2024-07-05 17:26:49 +0100

    IDVA6-1181 Utility function added to get user role tag to display

--- src/lib/utils/viewUtils.ts
+++ src/lib/utils/viewUtils.ts
@@ -1,7 +1,21 @@
+import { UserRoleTag } from "../../types/userRole";
+
 export const getLink = (href: string, displayText: string): string => {
     return `<a href="${href}">${displayText}</a>`;
 };
 
 export const getHiddenText = (hiddenText: string): string => {
     return `<span class="govuk-visually-hidden">${hiddenText}</span>`;
+};
+
+// Temporary function to be used until relevant API calls available
+export const getUserRoleTag = (userEmailAddress: string): string => {
+    switch (userEmailAddress) {
+    case "k.williams@example.com":
+        return UserRoleTag.ADMIN.toString();
+    case "j.smith@example.com":
+        return UserRoleTag.OWNER.toString();
+    default:
+        return UserRoleTag.STANDARD.toString();
+    }
 };
