69c92a5a87b21534db0e8b7755f63d407a84989f
Maciej Modzelewski <150266545+mmodzelewski-ch@users.noreply.github.com>
2024-07-08 16:58:21 +0100

    IDVA6-1181 Hard coded data removed and return type added

--- src/routers/controllers/checkMemberDetailsController.ts
+++ src/routers/controllers/checkMemberDetailsController.ts
@@ -1,18 +1,18 @@
-import { Request, RequestHandler, Response } from "express";
+import { Request, Response } from "express";
 import * as constants from "../../lib/constants";
 import { AnyRecord } from "../../types/utilTypes";
 import { getTranslationsForView } from "../../lib/utils/translationUtils";
 import { getExtraData } from "../../lib/utils/sessionUtils";
 import { getUserRoleTag } from "../../lib/utils/viewUtils";
 
-export const checkMemberDetailsControllerGet: RequestHandler = async (req: Request, res: Response) => {
+export const checkMemberDetailsControllerGet = async (req: Request, res: Response): Promise<void> => {
     const viewData = getViewData(req);
     res.render(constants.CHECK_MEMBER_DETAILS_PAGE, { ...viewData });
 };
 
 const getViewData = (req: Request): AnyRecord => {
     const translations = getTranslationsForView(req.t, constants.CHECK_MEMBER_DETAILS_PAGE);
-    const userEmailAddress = getExtraData(req.session, constants.EMAIL_OF_USER_TO_ADD) || "a.brown@test.com";
+    const userEmailAddress = getExtraData(req.session, constants.EMAIL_OF_USER_TO_ADD);
     // Hardcoded data will be replaced once relevant API calls available
     const companyName = "MORRIS ACCOUNTING LTD";
     const userRole = getUserRoleTag(userEmailAddress);
