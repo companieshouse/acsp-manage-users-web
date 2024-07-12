33d45f3ef0a8375d9eed5acfcf77f550216cd4a1
bwallace-ch <bwallace@companieshouse.gov.uk>
2024-07-12 09:01:22 +0100

    Adding new interface and updating manageUsers controller

--- src/routers/controllers/removeUserCheckDetailsController.ts
+++ src/routers/controllers/removeUserCheckDetailsController.ts
@@ -2,9 +2,8 @@ import { Request, Response } from "express";
 import * as constants from "../../lib/constants";
 import { getTranslationsForView } from "../../lib/utils/translationUtils";
 import { AnyRecord } from "types/utilTypes";
-import { NewUserDetails } from "../../types/user";
-import { getExtraData, setExtraData } from "../../lib/utils/sessionUtils";
-import { UserRole } from "private-api-sdk-node/dist/services/acsp-manage-users/types";
+import { getExtraData } from "../../lib/utils/sessionUtils";
+import { Membership } from "../../types/membership";
 
 export const removeUserCheckDetailsControllerGet = async (req: Request, res: Response): Promise<void> => {
     const viewData = getViewData(req);
@@ -15,17 +14,21 @@ const getViewData = (req: Request): AnyRecord => {
     const translations = getTranslationsForView(req.t, constants.REMOVE_MEMBER_PAGE);
 
     // Hardcoded data will be replaced once relevant API calls available
-    // const userRole: UserRole = UserRole.STANDARD;
-    // const userDetails: NewUserDetails = { userRole: userRole, userId: "12345", isValid: true, email: "d.jones@example.com", userName: "Davy Jones" };
-    // setExtraData(req.session, constants.DETAILS_OF_USER_TO_REMOVE, userDetails);
+    const membership = {
+        id: "111111",
+        userId: "12345",
+        userEmail: "james.morris@gmail.com",
+        displayUserName: "James Morris",
+        AcspNumber: "B149YU"
+    } as Membership;
 
-    const newUserDetails: NewUserDetails = getExtraData(req.session, constants.DETAILS_OF_USER_TO_REMOVE);
+    const removeUserDetails: Membership = getExtraData(req.session, constants.MANAGE_USERS_MEMBERSHIP);
     const companyName = "MORRIS ACCOUNTING LTD";
 
     return {
         lang: translations,
         companyName,
-        newUserDetails,
+        removeUserDetails,
         cancelLinkHref: constants.MANAGE_USER_FULL_URL,
         backLinkUrl: constants.MANAGE_USER_FULL_URL,
         removeButtonHref: constants.CONFIRMATION_MEMBER_REMOVED_FULL_URL
