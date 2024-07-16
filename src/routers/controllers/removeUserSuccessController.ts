6e8c7c4c36e6fca735342b737707967ec6b57bf7
bwallace-ch <bwallace@companieshouse.gov.uk>
2024-07-16 09:36:40 +0100

    IDVA6-1198 - updating unit tests and njk file to account for user object

--- src/routers/controllers/removeUserSuccessController.ts
+++ src/routers/controllers/removeUserSuccessController.ts
@@ -3,8 +3,7 @@ import * as constants from "../../lib/constants";
 import { getTranslationsForView } from "../../lib/utils/translationUtils";
 import { AnyRecord } from "types/utilTypes";
 import { NewUserDetails } from "../../types/user";
-import { getExtraData, setExtraData } from "../../lib/utils/sessionUtils";
-import { UserRole } from "private-api-sdk-node/dist/services/acsp-manage-users/types";
+import { getExtraData } from "../../lib/utils/sessionUtils";
 
 export const removeUserSuccessControllerGet = async (req: Request, res: Response): Promise<void> => {
     const viewData = getViewData(req);
@@ -13,12 +12,6 @@ export const removeUserSuccessControllerGet = async (req: Request, res: Response
 
 const getViewData = (req: Request): AnyRecord => {
     const translations = getTranslationsForView(req.t, constants.USER_REMOVE_CONFIRMATION_PAGE);
-
-    // Hardcoded data will be replaced once relevant API calls available
-    // const userRole: UserRole = UserRole.STANDARD;
-    // const userDetails: NewUserDetails = { userRole: userRole, userId: "12345", isValid: true, email: "d.jones@example.com", userName: "Davy Jones" };
-    // setExtraData(req.session, constants.DETAILS_OF_USER_TO_REMOVE, userDetails);
-
     const newUserDetails: NewUserDetails = getExtraData(req.session, constants.DETAILS_OF_USER_TO_REMOVE);
     const companyName = "MORRIS ACCOUNTING LTD";
 
