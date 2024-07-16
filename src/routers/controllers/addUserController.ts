c7b32dcda0458eff1088bcee4888d578712fd889
Maciej Modzelewski <150266545+mmodzelewski-ch@users.noreply.github.com>
2024-07-16 09:39:31 +0100

    IDVA6-1188 Radio buttons displayed based on the role of the logged in user

--- src/routers/controllers/addUserController.ts
+++ src/routers/controllers/addUserController.ts
@@ -1,17 +1,20 @@
 import { Request, Response } from "express";
 import * as constants from "../../lib/constants";
 import { getTranslationsForView } from "../../lib/utils/translationUtils";
-import { setExtraData, getExtraData } from "../../lib/utils/sessionUtils";
+import { setExtraData, getExtraData, getLoggedInUserEmail } from "../../lib/utils/sessionUtils";
 import { ViewData } from "../../types/utilTypes";
 import { clearFormSessionValues } from "../../lib/validation/clear.form.validation";
 import { validateAndSetErrors } from "../../lib/validation/add.user.validation";
 import { NewUserDetails } from "../../types/user";
+import { UserRole } from "private-api-sdk-node/dist/services/acsp-manage-users/types";
 
 export const addUserControllerGet = async (req: Request, res: Response): Promise<void> => {
+    const loggedInUserRole = getUserRole(getLoggedInUserEmail(req.session));
     const viewData: ViewData = {
         lang: getTranslationsForView(req.t, constants.ADD_USER_PAGE),
         companyName: "MORRIS ACCOUNTING LTD",
-        backLinkUrl: constants.MANAGE_USER_FULL_URL
+        backLinkUrl: constants.MANAGE_USER_FULL_URL,
+        loggedInUserRole
     };
     clearFormSessionValues(req, constants.DETAILS_OF_USER_TO_ADD);
     const savedNewUserDetails = getExtraData(
@@ -34,13 +37,15 @@ export const addUserControllerGet = async (req: Request, res: Response): Promise
 export const addUserControllerPost = async (req: Request, res: Response): Promise<void> => {
     const email = req.body.email.trim();
     const userRole = req.body.userRole;
+    const loggedInUserRole = getUserRole(getLoggedInUserEmail(req.session));
 
     const viewData: ViewData = {
         lang: getTranslationsForView(req.t, constants.ADD_USER_PAGE),
         companyName: "MORRIS ACCOUNTING LTD",
         backLinkUrl: constants.MANAGE_USER_FULL_URL,
         email,
-        userRole
+        userRole,
+        loggedInUserRole
     };
 
     validateAndSetErrors(email, userRole, viewData);
@@ -51,5 +56,17 @@ export const addUserControllerPost = async (req: Request, res: Response): Promis
     } else {
         setExtraData(req.session, constants.DETAILS_OF_USER_TO_ADD, { email, userRole, isValid: true } as unknown as NewUserDetails);
         return res.redirect(constants.CHECK_MEMBER_DETAILS_FULL_URL);
+    }
+};
+
+// Temporary function until relevant API available
+const getUserRole = (userEmailAddress: string): UserRole => {
+    switch (userEmailAddress) {
+    case "demo@ch.gov.uk":
+        return UserRole.OWNER;
+    case "demo2@ch.gov.uk":
+        return UserRole.ADMIN;
+    default:
+        return UserRole.STANDARD;
     }
 };
