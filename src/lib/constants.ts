4f3b6b93e7ac7c92d77a3f6f812aabf3eddf95e2
Maciej Modzelewski <150266545+mmodzelewski-ch@users.noreply.github.com>
2024-08-09 16:50:27 +0100

    IDVA6-1363 URL constant name changed and items per page default constant added

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -112,3 +112,4 @@ export const ACCOUNT_OWNERS_TAB_ID = `tab_${ACCOUNT_OWNERS_ID}`;
 export const ADMINISTRATORS_TAB_ID = `tab_${ADMINISTRATORS_ID}`;
 export const STANDARD_USERS_TAB_ID = `tab_${STANDARD_USERS_ID}`;
 export const NOT_PROVIDED = "Not Provided";
+export const ITEMS_PER_PAGE_DEFAULT = 15;
