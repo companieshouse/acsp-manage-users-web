1e83107fd5d869d57376be6dd7283e0e1e40da30
bwallace-ch <151055584+bwallace-ch@users.noreply.github.com>
2024-11-06 15:57:33 +0000

    Revert "IDVA6-1637 - Edit user role - Manage users journey - Redirections"

--- src/lib/utils/urlUtils.ts
+++ src/lib/utils/urlUtils.ts
@@ -6,6 +6,6 @@ const WHITELISTED_URLS: string[] = [
 
 export const isWhitelistedUrl = (url: string): boolean => WHITELISTED_URLS.includes(url);
 
-export const getRemoveMemberCheckDetailsFullUrl = (id: string): string => `${constants.REMOVE_MEMBER_BASE}/${id}`;
+export const getRemoveMemberCheckDetailsFullUrl = (id: string): string => `${constants.LANDING_URL}/${constants.REMOVE_MEMBER_PAGE}/${id}`;
 
-export const getChangeMemberRoleFullUrl = (id: string): string => `${constants.CHANGE_MEMBER_ROLE_BASE}/${id}`;
+export const getChangeMemberRoleFullUrl = (id: string): string => `${constants.LANDING_URL}/${constants.EDIT_MEMBER_ROLE_PAGE}/${id}`;
