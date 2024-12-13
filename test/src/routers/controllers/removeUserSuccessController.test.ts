65939402c9ef8f7388c87a0ee74ed3210d92620b
bwallace-ch <bwallace@companieshouse.gov.uk>
2024-12-13 11:29:55 +0000

    IDVA6-1621 - updating confirmation member removed page content and tests

--- test/src/routers/controllers/removeUserSuccessController.test.ts
+++ test/src/routers/controllers/removeUserSuccessController.test.ts
@@ -3,6 +3,8 @@ import supertest from "supertest";
 import app from "../../../../src/app";
 import * as en from "../../../../locales/en/confirmation-member-removed.json";
 import * as enCommon from "../../../../locales/en/common.json";
+import * as cy from "../../../../locales/cy/confirmation-member-removed.json";
+import * as cyCommon from "../../../../locales/cy/common.json";
 import * as constants from "../../../../src/lib/constants";
 import { setExtraData } from "../../../../src/lib/utils/sessionUtils";
 import * as sessionUtils from "../../../../src/lib/utils/sessionUtils";
@@ -62,11 +64,34 @@ describe("GET /authorised-agent/confirmation-member-removed", () => {
         const response = await router.get(url);
 
         // Then
-        expect(response.text).toContain(`${en.you_have_removed}${userDetails.userDisplayName}`);
+        expect(response.text).toContain(`${en.user_removed}`);
+        expect(response.text).toContain(`${en.you_have_removed}`);
+        expect(response.text).toContain(`${userDetails.userDisplayName}`);
         expect(response.text).toContain(`${en.from}${companyName}`);
         expect(response.text).toContain(en.what_happens_now_they_have_been_removed);
-        expect(response.text).toContain(`${userDetails.userDisplayName}${en.will_no_longer_be_able_to_access}${companyName}`);
+        expect(response.text).toContain(`${en.will_no_longer_be_able_to_access}${companyName}`);
         expect(response.text).toContain(`${enCommon.go_to_manage_users}`);
+    });
+
+    it("should return expected Welsh content if person has been removed and userName is provided", async () => {
+
+        // Given
+        session.setExtraData("lang", "cy");
+
+        getLoggedUserAcspMembershipSpy.mockReturnValue(loggedInUserMembership);
+        setExtraData(session, constants.DETAILS_OF_USER_TO_REMOVE, userDetails);
+
+        // When
+        const response = await router.get(url);
+
+        // Then
+        expect(response.text).toContain(`${cy.user_removed}`);
+        expect(response.text).toContain(`${cy.you_have_removed}`);
+        expect(response.text).toContain(`${userDetails.userDisplayName}`);
+        expect(response.text).toContain(`${cy.from}${companyName}`);
+        expect(response.text).toContain(cy.what_happens_now_they_have_been_removed);
+        expect(response.text).toContain(`${cy.will_no_longer_be_able_to_access}${companyName}`);
+        expect(response.text).toContain(`${cyCommon.go_to_manage_users}`);
     });
 
     it("should return expected English content if person has been removed and userName is not provided", async () => {
@@ -88,10 +113,12 @@ describe("GET /authorised-agent/confirmation-member-removed", () => {
         const response = await router.get(url);
 
         // Then
-        expect(response.text).toContain(`${en.you_have_removed}${userDetails.displayNameOrEmail}`);
+        expect(response.text).toContain(`${en.user_removed}`);
+        expect(response.text).toContain(`${en.you_have_removed}`);
+        expect(response.text).toContain(`${userDetails.displayNameOrEmail}`);
         expect(response.text).toContain(`${en.from}${companyName}`);
         expect(response.text).toContain(en.what_happens_now_they_have_been_removed);
-        expect(response.text).toContain(`${userDetails.displayNameOrEmail}${en.will_no_longer_be_able_to_access}${companyName}`);
+        expect(response.text).toContain(`${en.will_no_longer_be_able_to_access}${companyName}`);
         expect(response.text).toContain(`${enCommon.go_to_manage_users}`);
     });
 });
