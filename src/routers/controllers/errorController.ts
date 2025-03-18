efe06dfc243e4854f8883f440eb4cb914587a8fc
mmodzelewski-ch <mmodzelewski@companieshouse.gov.uk>
2025-03-18 11:31:01 +0000

    IDVA6-2342 Unused import removed

--- src/routers/controllers/errorController.ts
+++ src/routers/controllers/errorController.ts
@@ -1,7 +1,6 @@
 import logger from "../../lib/Logger";
 import type { ErrorRequestHandler } from "express";
 import { HttpError } from "http-errors";
-import { getTranslationsForView } from "../../lib/utils/translationUtils";
 import * as constants from "../../lib/constants";
 import { CsrfError, InvalidAcspNumberError } from "@companieshouse/web-security-node";
 
