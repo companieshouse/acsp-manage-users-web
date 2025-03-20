d091648c9e056dc6d94028750f7f662b4b0b51c8
kwamenyantakyi-ch <knyantakyi@companieshouse.gov.uk>
2025-03-20 19:37:59 +0000

    IDVA6-1351 unit tests

--- test/src/routers/controllers/accessibilityStatmentAuthorisedAgentController.test.ts
+++ test/src/routers/controllers/accessibilityStatmentAuthorisedAgentController.test.ts
@@ -1,19 +1,10 @@
 import { Request, Response } from "express";
 import { accessibilityStatementAuthorisedAgentControllerGet } from "../../../../src/routers/controllers/accessibilityStatmentAuthorisedAgentController";
 import * as constants from "../../../../src/lib/constants";
-import * as en from "../../../../locales/en/accessibility-statement.json";
 import * as getTranslationsForView from "../../../../src/lib/utils/translationUtils";
-import * as sessionUtils from "../../../../src/lib/utils/sessionUtils";
-import supertest from "supertest";
-import app from "../../../../src/app";
-import { session } from "../../../mocks/session.middleware.mock";
-
-const router = supertest(app);
-const url = "/authorised-agent/accessibility-statement";
 
 describe("accessibilityStatementAuthorisedAgentControllerGet", () => {
     const mockGetTranslationsForView = jest.spyOn(getTranslationsForView, "getTranslationsForView");
-    const getLoggedUserAcspMembershipSpy: jest.SpyInstance = jest.spyOn(sessionUtils, "getLoggedUserAcspMembership");
 
     let req: Partial<Request>;
     let res: Partial<Response>;
@@ -30,10 +21,7 @@ describe("accessibilityStatementAuthorisedAgentControllerGet", () => {
         } as Partial<Response>;
 
         jest.clearAllMocks();
-    });
 
-    it("should return status 200", async () => {
-        await router.get(url).expect(200);
     });
 
     it("should render the accessibility statement page with correct translations", async () => {
