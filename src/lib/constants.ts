e2e6ed4d65ff77ec6baa24e6c59d68d20afe685a
bmchugh-ch <bmchugh@companieshouse.gov.uk>
2025-03-24 18:58:36 +0000

    SIV-136 add correct url, and unit tests

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -27,7 +27,7 @@ export const VALIDATION_ERRORS = "VALIDATION_ERRORS";
 // External URLs
 export const CHS_SEARCH_REGISTER_PAGE = "https://find-and-update.company-information.service.gov.uk/";
 export const UPDATE_AUTHORISED_AGENTS_DETAILS_URL = "/placeholder-url-to-service-build-by-other-team";
-export const YOU_HAVE_VERIFIED_SOMEONES_IDENTITY_URL = "/update-this-please";
+export const YOU_HAVE_VERIFIED_SOMEONES_IDENTITY_URL = "/tell-companies-house-you-have-verified-someones-identity";
 export const SIGN_OUT_URL = `${CHS_URL}/signout`;
 
 // Translation and Template Keys
