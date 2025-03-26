4cded6ac5d04d3728b3a3ef5f5169821322aabf1
mmodzelewski-ch <mmodzelewski@companieshouse.gov.uk>
2025-03-26 13:56:32 +0000

    SIV-129 Unit tests added for api client service

--- test/src/services/apiClientService.test.ts
+++ test/src/services/apiClientService.test.ts
@@ -1,8 +1,16 @@
 import { createPrivateApiClient } from "private-api-sdk-node";
 import PrivateApiClient from "private-api-sdk-node/dist/client";
-import { createPrivateApiKeyClient } from "../../../src/services/apiClientService";
-import { IHttpClient } from "@companieshouse/api-sdk-node";
+import { createOAuthApiClient, createOauthPrivateApiClient, createPrivateApiKeyClient } from "../../../src/services/apiClientService";
+import { createApiClient, IHttpClient } from "@companieshouse/api-sdk-node";
+import * as sessionUtils from "../../../src/lib/utils/sessionUtils";
+import { mockRequest } from "../../mocks/request.mock";
+import { Request } from "express";
+import { Session } from "@companieshouse/node-session-handler";
+import ApiClient from "@companieshouse/api-sdk-node/dist/client";
 jest.mock("private-api-sdk-node");
+jest.mock("@companieshouse/api-sdk-node");
+
+const getAccessTokenSpy: jest.SpyInstance = jest.spyOn(sessionUtils, "getAccessToken");
 
 describe("createPrivateApiKeyClient", () => {
     it("should return private API client", () => {
@@ -13,5 +21,30 @@ describe("createPrivateApiKeyClient", () => {
         const result = createPrivateApiKeyClient();
         // Then
         expect(result).toBeInstanceOf(PrivateApiClient);
+    });
+});
+
+describe("createOauthPrivateApiClient", () => {
+    it("should return private API client", () => {
+        // Given
+        const privateApiClient: PrivateApiClient = new PrivateApiClient({} as IHttpClient, {} as IHttpClient);
+        (createPrivateApiClient as jest.Mock).mockReturnValue(privateApiClient);
+        getAccessTokenSpy.mockReturnValue("accessToken");
+        // When
+        const result = createOauthPrivateApiClient(mockRequest() as Request);
+        // Then
+        expect(result).toBeInstanceOf(PrivateApiClient);
+    });
+});
+
+describe("createOAuthApiClient", () => {
+    it("should return private API client", () => {
+        // Given
+        const apiClient: ApiClient = new ApiClient({} as IHttpClient, {} as IHttpClient);
+        (createApiClient as jest.Mock).mockReturnValue(apiClient);
+        // When
+        const result = createOAuthApiClient(new Session());
+        // Then
+        expect(result).toBeInstanceOf(ApiClient);
     });
 });
