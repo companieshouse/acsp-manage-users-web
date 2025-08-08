bc42ef2eb8f2088e44fc17e340bf0c3f513e04af
bmchugh-ch <bmchugh@companieshouse.gov.uk>
2025-08-08 11:22:04 +0100

    SIV-524 allow form src to be redirected anywhere when signout

--- src/middleware/content.security.policy.middleware.config.ts
+++ src/middleware/content.security.policy.middleware.config.ts
@@ -1,5 +1,4 @@
 import { HelmetOptions } from "helmet";
-import { CHS_URL, ACCOUNT_URL } from "../lib/constants";
 
 export const prepareCSPConfig = (nonce: string): HelmetOptions => {
     const CDN = process.env.CDN_HOST as string;
@@ -20,9 +19,7 @@ export const prepareCSPConfig = (nonce: string): HelmetOptions => {
                 connectSrc: [SELF, PIWIK_URL, CDN],
                 formAction: [
                     SELF,
-                    PIWIK_CHS_DOMAIN,
-                    CHS_URL,
-                    ACCOUNT_URL
+                    PIWIK_CHS_DOMAIN
                 ],
                 scriptSrc: [
                     NONCE,
