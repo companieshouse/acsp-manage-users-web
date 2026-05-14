2ed24ee352e35370f6034a5ce80124af4546eabf
kieo-ch <koleary1@companieshouse.gov.uk>
2026-05-14 16:28:32 +0100

    Add initial poc work

--- src/middleware/loggedUserAcspMembership.middleware.ts
+++ src/middleware/loggedUserAcspMembership.middleware.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
-import { getLoggedUserAcspMembership, setExtraData } from "../lib/utils/sessionUtils";
-import { getMembershipForLoggedInUser } from "../services/acspMemberService";
+import { getLoggedUserAcspMembership, setExtraData, userHasPermission } from "../lib/utils/sessionUtils";
+import { getAcspMemberships, getMembershipForLoggedInUser } from "../services/acspMemberService";
 import { AcspMembership, AcspStatus } from "private-api-sdk-node/dist/services/acsp-manage-users/types";
 import * as constants from "../lib/constants";
 import { isWhitelistedUrl } from "../lib/utils/urlUtils";
@@ -24,7 +24,12 @@ export const loggedUserAcspMembershipMiddleware = async (
     if (!acspMembershipInSession || onDashboard) {
         logger.info("Fetching / updating ACSP membership for logged in user");
         const membershipResponse = await getMembershipForLoggedInUser(req);
-        const membership = membershipResponse?.items?.[0];
+        let membership = membershipResponse?.items?.[0];
+
+        if (userHasPermission(req.session, "/admin/acsp/manage")) {
+            logger.info("User has CH internal admin permissions, setting admin ACSP membership in session");
+            membership = await constructAdminAcspMembership(req);
+        }
 
         if (!membership) {
             logger.error("No membership found for logged in user");
@@ -41,4 +46,22 @@ export const loggedUserAcspMembershipMiddleware = async (
 
     return next();
 
+};
+
+const constructAdminAcspMembership = async (req: Request): Promise<AcspMembership> => {
+    // Retrieve ACSP number from session and use it to construct a temporary ACSP membership with 
+    // owner role for users with CH internal admin permissions
+    const acspNumber = req?.session?.data?.signin_info?.acsp_number!;
+
+    // Retrieve ACSP name from any of the ACSP memberships
+    const ownerMembers = await getAcspMemberships(req, acspNumber, false);
+    const acspName = ownerMembers?.items?.[0]?.acspName!;
+
+    const adminMembership: AcspMembership = {
+        acspNumber,
+        acspName,
+        userRole: "owner",
+        acspStatus: AcspStatus.ACTIVE,
+    } as AcspMembership;
+    return adminMembership;
 };
